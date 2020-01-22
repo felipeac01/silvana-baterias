c96a01d5e9e728a667ca29e471f3f3f380a190d3
felipeac01 <2019.felipealcantara@gmail.com>
2020-01-21 23:54:58 -0200

    HORROROSO

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Silvana Baterias</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/estilo.css">
+	<!--[if lt IE 9]>
+		<script src="html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header id="header">
+		<div id="header-linha1">
+			<nav id="menu"></nav>
+			<div id="logo"></div>
+			<div id="busca">
+				<input type="text" placeholder="Pesquisar...">
+			</div>
+			<div id="carrinho"></div>
+			<div class="clear"></div>
+		</div>
+		<div id="header-linha2">
+			ITEM 1 | ITEM 2 | ETC
+		</div>
+	</header>
+	<main>
+	</main>
+</body>
+</html>
